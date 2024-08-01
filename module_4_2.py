df8062f1b6dbac8fc7f2540c434be818a749b815
AleksandraM <musatova.aleksandra@mail.ru>
2024-08-01 16:11:04 +0300

    modul_4.1

---
+++ module_4_2.py
@@ -0,0 +1,7 @@
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function')
+    inner_function()
+
+test_function() # При вызове функции test_function внутреняя функция inner_function работает коректно
+inner_function() # При попытке вызвать фукцию отдельно выходит ошибка, что функция не определена
