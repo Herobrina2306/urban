b90f6fe989eefab69754c1b5cb701b78b30e9858
AleksandraM <musatova.aleksandra@mail.ru>
2024-08-06 16:18:50 +0300

    modul_5.1

---
+++ module_5_1.py
@@ -0,0 +1,20 @@
+class House:
+    def __init__(self, name, number_of_floor):
+        self.name = name
+        self.number_of_floor = number_of_floor
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floor or new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            count = 1
+            while count <= new_floor:
+                print(count)
+                count += 1
+
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
