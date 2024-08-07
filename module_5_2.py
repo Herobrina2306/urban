840e322389081bca0d0288009b8b037222e5e6e2
AleksandraM <musatova.aleksandra@mail.ru>
2024-08-07 09:53:14 +0300

    modul_5.2

---
+++ module_5_2.py
@@ -0,0 +1,37 @@
+class House:
+    def __init__(self, name, number_of_floor):
+        self.name = name
+        self.number_of_floor = number_of_floor
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floor or new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            count = 1
+            while count <= new_floor:
+                print(count)
+                count += 1
+
+    def __len__(self):
+        return self.number_of_floor
+
+    def __str__(self):
+        return f'Название: {self.name}, количество этажей: {self.number_of_floor}'
+
+
+
+# h1 = House('ЖК Горский', 18)
+# h2 = House('Домик в деревне', 2)
+# h1.go_to(5)
+# h2.go_to(10)
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
