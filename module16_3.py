793380e913ae3c1ccad5a9f77e918c6df70fbd26
AleksandraM <musatova.aleksandra@mail.ru>
2024-12-29 11:48:10 +0300

    module16_5

--- module16_3.py
+++ module16_3.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Request
+from fastapi.responses import HTMLResponse
 from typing import Annotated, List
 from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 users = []
 
@@ -13,9 +17,12 @@ class User(BaseModel):
     age: int
 
 
-@app.get('/users')
-async def get_dict() -> List[User]:
-    return users
+@app.get("/user/{user_id}", response_class=HTMLResponse)
+async def get_task(request: Request, user_id: Annotated[int, Path(ge=1)]):
+    for user in users:
+        if user.id == user_id:
+            return templates.TemplateResponse("users.html", {"request": request, "user": user})
+    raise HTTPException(status_code=404, detail="Task not found")
 
 
 @app.post('/user/{username}/{age}')
@@ -48,3 +55,8 @@ async def delete_user(user_id: Annotated[int, Path(description="Enter id", examp
         return us
     except IndexError:
         raise HTTPException(status_code=404, detail="User was not found")
+
+
+@app.get('/', response_class=HTMLResponse)
+async def all_users(request: Request):
+    return templates.TemplateResponse("users.html", {"request": request, "users": users})
